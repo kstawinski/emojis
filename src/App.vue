f128b5f4404b3f63bb50f05af49ed7adc7aee412
kstawinski <stawinskidev@gmail.com>
2020-05-14 15:59:49 +0200

    Add font

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,12 @@ export default {
 
 <style lang="scss">
 @import url('assets/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap');
+
+body {
+  font-family: 'Rubik', sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
 
 .container {
   width: 90%;
