5b8e11e53e9e5e25a778d4455a533d49022e74ab
kstawinski <stawinskidev@gmail.com>
2020-05-14 16:17:28 +0200

    Add basic view of Emoji component

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,16 +1,30 @@
 <template>
   <main class="content">
-    <div class="container">
-      content
+    <div class="container content_container">
+      <Emoji v-for="emoji in emojis" :char="emoji" :key="emoji" />
     </div>
   </main>
 </template>
 
 <script>
+import Emoji from '@/components/Emoji.vue';
+
 export default {
   name: 'Content',
+  components: { Emoji },
+  data() {
+    return {
+      emojis: ['😍', '😎', '😜', '🙄', '🙈'],
+    };
+  },
 };
 </script>
 
 <style scoped lang="scss">
+.content {
+  &_container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
 </style>
---
+++ src/components/Emoji.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="emoji">{{ char }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Emoji',
+  props: {
+    char: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.emoji {
+  font-size: 3em;
+  cursor: pointer;
+  transition: 0.15s all;
+  margin: 5px;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+}
+</style>
