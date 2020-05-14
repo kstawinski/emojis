e0247cc6e28ff440ce145b74ee8b9b138f55d1d6
kstawinski <stawinskidev@gmail.com>
2020-05-14 15:57:49 +0200

    Create prototype of main content component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <Header />
+    <Content />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
+import Content from '@/components/Content.vue';
 
 export default {
   name: 'App',
-  components: { Header },
+  components: { Header, Content },
 };
 </script>
 
---
+++ src/components/Content.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="content">
+    <div class="container">
+      content
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+};
+</script>
+
+<style scoped lang="scss">
+</style>
