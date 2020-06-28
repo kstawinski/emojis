176728d597e87d87342fcd6b2a8fcfda123855ba
kstawinski <stawinskidev@gmail.com>
2020-06-28 17:02:42 +0200

    Remove button from Search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="search">
-    <button class="search_button button" @click="toggleInput()">
-      <img class="button_icon" src="../assets/search.svg" alt="Search icon">
-    </button>
-    <input v-if="showInput" type="text" class="search_input">
+    <input class="search_input" type="search">
   </div>
 </template>
 
@@ -11,15 +8,9 @@
 export default {
   name: 'Search',
   data() {
-    return {
-      showInput: false,
-    };
+    return {};
   },
-  methods: {
-    toggleInput() {
-      this.showInput = !this.showInput;
-    },
-  },
+  methods: {},
 };
 </script>
 
@@ -28,20 +19,6 @@ export default {
   display: flex;
   align-items: center;
 
-  &_button {
-    border: 0;
-    padding: 0;
-    outline: 0;
-    position: relative;
-
-      &:before {
-        content: '';
-        width: 30px;
-        height: 30px;
-        background: rgba(#000000, 0.05);
-        position: absolute;
-      }
-  }
   &_input {
     outline: 0;
     border: 0;
@@ -51,11 +28,6 @@ export default {
     color: gray;
     border-bottom: 2px solid rgba(#000000, 0.05);
     padding: 0 0 5px 0;
-  }
-}
-.button {
-  &_icon {
-    width: 35px;
   }
 }
 </style>
